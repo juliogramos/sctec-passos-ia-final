5dc415e0619e811dd85f9503ee14c46ab0fd9215
Julio <juliogoncalvesramos@yahoo.com.br>
2026-05-28 15:03:09 -0300

    outliers

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
-from utilitarios import ver_tabela_nulos
+from utilitarios import outlier_calc, ver_tabela_nulos
 
 # Carregando o arquivo
-# Agent e company serão convertidos para string pois são IDs e não devem ser
-# interpretados como valores numéricos
 df = pd.read_csv(
     "hotel_bookings.csv",
     encoding="latin_1",
-    dtype={
-        "agent": str,
-        "company": str,
-    },
 )
 
 # Visualização inicial dos dados
@@ -50,13 +46,13 @@ print("TRATAMENTO DE NULOS:")
 # Os nulos de company são quase todo o dataset e serão preenchidos com uma nova
 # categoria, ID -1
 print("Os nulos de company serão preenchidos por uma nova categoria -1.")
-df["company"] = df["company"].fillna("-1")
+df["company"] = df["company"].fillna(-1)
 print(f"Nulos restantes em company: {df['company'].isnull().sum()}", "\n")
 
 # Os nulos de agent podem ser interpretados como reservas sem agência, que é uma
 # opção válida. Preenchendo com ID -1
 print("Os nulos de agent serão preenchidos por uma nova categoria -1.")
-df["agent"] = df["agent"].fillna("-1")
+df["agent"] = df["agent"].fillna(-1)
 print(f"Nulos restantes em agent: {df['agent'].isnull().sum()}", "\n")
 
 # Os nulos de country e children serão removidos pois a falta desses valores
@@ -66,3 +62,78 @@ df = df.dropna(subset=["country", "children"])
 
 print("Nulos restantes:")
 ver_tabela_nulos(df)
+
+# Distribuição de features
+numeric_features = [
+    "lead_time",
+    "stays_in_weekend_nights",
+    "stays_in_week_nights",
+    "adults",
+    "children",
+    "babies",
+    "previous_cancellations",
+    "previous_bookings_not_canceled",
+    "booking_changes",
+    "days_in_waiting_list",
+    "adr",
+    "required_car_parking_spaces",
+    "total_of_special_requests",
+]
+
+categoric_features = [
+    "hotel",
+    "is_canceled",
+    "arrival_date_year",
+    "arrival_date_month",
+    "arrival_date_week_number",
+    "arrival_date_day_of_month",
+    "meal",
+    "country",
+    "market_segment",
+    "distribution_channel",
+    "is_repeated_guest",
+    "reserved_room_type",
+    "assigned_room_type",
+    "deposit_type",
+    "agent",
+    "company",
+    "customer_type",
+    "reservation_status",
+]
+
+# Verificação de outliers por box plots
+fig, axs = plt.subplots(nrows=2, ncols=7, figsize=(25, 25))
+axs = axs.flatten()
+fig.delaxes(axs[13])
+
+for i, col in enumerate(numeric_features):
+    sns.boxplot(y=df[col], ax=axs[i], color="skyblue")
+    axs[i].set_title(f"Box plot de {col}")
+    axs[i].set_xlabel("")
+
+plt.tight_layout()
+plt.show()
+
+# À primeira vista, apenas o total_of_special_requests é um bom candidato para
+# a remoção de outliers
+
+# Verificação de outliers por IQR
+print("IDENTIFICANDO OUTLIERS POR IQR:")
+outliers_list = []
+for col in numeric_features:
+    outliers, percentual = outlier_calc(df, col)
+    outliers_list.append([outliers, percentual])
+outliers_df = pd.DataFrame(
+    outliers_list, index=numeric_features, columns=["Total outliers", "Percentual (%)"]
+)
+outliers_df = outliers_df.sort_values(by="Total outliers", ascending=False)
+print(outliers_df)
+print(
+    f"Total de outliers: {outliers_df['Total outliers'].sum()}, {outliers_df['Percentual (%)'].sum():.2f} do dataset.",
+    "\n",
+)
+
+# Quais remover?
+# Por enquanto, não vou remover nenhum outlier, pois eles podem ter correlações
+# importantes para a modelagem preditiva.
+# Irei checar essas correlações no próximo passo.
--- utilitarios.py
+++ utilitarios.py
@@ -10,3 +10,13 @@ def ver_tabela_nulos(df: DataFrame):
         by="Total nulos", ascending=False
     )
     print(nulos_df, "\n")
+
+
+def outlier_calc(df: DataFrame, coluna: str):
+    Q1 = df[coluna].quantile(0.25)
+    Q3 = df[coluna].quantile(0.75)
+    IQR = Q3 - Q1
+    limite_inferior = Q1 - 1.5 * IQR
+    limite_superior = Q3 + 1.5 * IQR
+    outliers = df[(df[coluna] < limite_inferior) | (df[coluna] > limite_superior)]
+    return len(outliers), len(outliers) / len(df) * 100
